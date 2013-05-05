c38ba118574f9cff3d0eb19c5e655410108f3b5b
Nikhil Benesch <me@designbynikhil.com>
2013-05-05 17:10:34 -0400

    move mashup test to tests

--- mashup.py
+++ mashup.py
@@ -40,16 +40,3 @@ def construct(wavs, segments):
 		ntracks[start:end] += 1
 	ntracks[ntracks == 0] = 1
 	return (samples // ntracks).astype(numpy.int16)
-
-def test():
-	wav1 = wav.Wav('data/for the first time.wav')
-	wav2 = wav.Wav('data/i knew you were trouble.wav')
-	wav3 = wav.Wav('data/all i need.wav')
-	wavs = [wav1, wav2, wav3]
-
-	segments = [
-		(0, 2 * SEC, 25 * SEC, 17 * SEC, 0.8),
-		(1, 7 * SEC, 25 * SEC, 1 * SEC, 0.4),
-		(2, 19 * SEC, 25 * SEC, 33 * SEC, 1.3)
-	]
-	wav.write('out.wav', construct(wavs, segments), SEC)
