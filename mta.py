fix zscore calculation for mta

mta.py
@@ -4,7 +4,7 @@ import numpy as np
 import sys
 
 match_threshold = 2
-symbol_list = np.array(map(chr, np.arange(97, 105)))
+symbol_list = [map(chr, range(97, 105))]
 num_symbols = len(symbol_list)
 PAA_interval = 1
 
@@ -13,14 +13,15 @@ class tracker:
 		self.word = np.array(word)
 		self.loc = np.array([],dtype='object')
 
-def mean (lst) : return sum(lst) / len (lst)
+def mean(lst): 
+	return sum(lst) / len(lst)
 
-def get_motifs(time_series_data):
+def get_motifs(time_series):
 	"""Runs MTA on time-series data represented as a list of floats.
 	Returns a list of segments-(start, end) tuples-identified as possible motifs.
 	"""
 
-	params = _convert_time_series(time_series_data)
+	params = _convert_time_series(time_series)
 	symbol_matrix = _generate_symbol_matrix(*params)
 	tracker_list = _initialize_tracker_population()
 	mutation_template = tracker_list
@@ -48,11 +49,11 @@ def _convert_time_series(time_series):
 	intervals.
 	"""
 	differential = np.subtract(time_series[1:], time_series[:-1])
-	diff_sig = stats.tstd(differential)
-	diff_mean = stats.tmean(differential)
-	norm_differential = (differential - diff_mean)/diff_sig
-	ntrackers = num_symbols
-	percentiles = np.arange(ntrackers + 1) * (100. / ntrackers)
+	std_dev = np.std(differential)
+	mean = np.mean(differential)
+	norm_differential = (differential - mean) / std_dev
+	
+	percentiles = np.arange(num_symbols + 1) * (100. / num_symbols)
 	zscores = np.array([stats.scoreatpercentile(norm_differential, p) for p in percentiles])
 
 	return norm_differential, zscores
@@ -71,6 +72,7 @@ def _generate_symbol_matrix(differential, zscores):
 				lower, upper = zscores[idx:idx+2] 
 				if lower <= mean(differential[i:i+PAA_interval]) <= upper:
 					symbol_matrix[i] = [symbol_list[idx], i]
+	print symbol_matrix
 	return symbol_matrix
 
 